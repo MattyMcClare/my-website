60ba072d0058ec4fd151668cb8a90ab4918f9f35
Mateusz <matstanczak@gmail.com>
2019-06-21 21:07:40 +0100

    created navbar

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -1,18 +1,23 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch, Redirect } from "react-router-dom";
-import Home from '../components/Home'
+import Home from '../components/Home';
+import About from '../components/About';
+import Portfolio from '../components/Portfolio';
+import NavBar from '../components/NavBar';
 
 class Main extends Component {
     render() {
         return (
         <Router>
+            <NavBar />
             <Switch>
-                <Route  path="/" component={Home}/>
+                <Route exact path="/" component={Home}/>
+                <Route path="/portfolio" component={Portfolio}/>
+                <Route path="/about" component={About} />
             </Switch>
         </Router>
-            )
+        )
     }
-    
 }
 
 export default Main;
